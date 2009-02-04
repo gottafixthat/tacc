1e56ea1044d457d15298d2d55ac874a737da90b8
marc <marc@f0b7a862-a5e2-0310-b556-ed4b80d0eeb2>
2009-02-04 00:09:42 +0000

    Added new CSV file parser.  Started with the EXPERIMENTAL flag that will remove certain code that is not yet completed.

---
+++ lib/CSVParser.h
@@ -0,0 +1,43 @@
+/*
+** CSVParser.h - Class definition for the CSVParser class
+****************************************************************************
+** Written by R. Marc Lewis, 
+**   (C)opyright 1998-2009, R. Marc Lewis and Avvatel Corporation
+**   All Rights Reserved.
+**
+**  Unpublished work.  No portion of this file may be reproduced in whole
+**  or in part by any means, electronic or otherwise, without the express
+**  written consent of Avvatel Corporation and R. Marc Lewis.
+****************************************************************************/
+
+#ifndef CSVPARSER_H
+#define CSVPARSER_H
+
+#include <qstring.h>
+#include <qfile.h>
+#include <qtextstream.h>
+#include <qstringlist.h>
+
+class CSVParser
+{
+public:
+    CSVParser();
+    ~CSVParser();
+
+    bool    openFile(const QString &fName, bool firstRowIsHeader = false);
+    void    setDelimiter(QChar &newDelim);
+    Q_LONG  loadRecord();
+    const QStringList &row();
+    const QStringList &header();
+
+protected:
+    QString     substSep(const QString &src, const QChar &srch, const QChar &repl);
+    QFile       myFile;
+    QTextStream *myStream;
+    QChar       myDelim;
+    QStringList curRow;
+    QStringList myHeader;
+
+};
+
+#endif
