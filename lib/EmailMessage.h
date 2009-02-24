961a5789d4bae7d148f6eda5ab4e66697bbdc348
marc <marc@f0b7a862-a5e2-0310-b556-ed4b80d0eeb2>
2009-02-24 18:36:30 +0000

    More work done on the EmailMessage class. Fixed the non-virtual destructor in QSqlDbPool class

--- lib/EmailMessage.h
+++ lib/EmailMessage.h
@@ -12,4 +12,48 @@
  *   written consent of Avvatel Corporation and R. Marc Lewis.
  */
 
+#ifndef EMAILMESSAGE_H
+#define EMAILMESSAGE_H
+
+#include <qstring.h>
+#include <qptrlist.h>
+#include <qcstring.h>
+
+struct emailAttachmentRecord {
+    QString     filename;
+    QString     mimeFilename;
+    QString     mimeType;
+    int         isTempFile;
+};
+
+class EmailMessage {
+
+public:
+    EmailMessage();
+    virtual ~EmailMessage();
+
+    void        clear();
+    void        setFrom(const char *);
+    void        setFrom(const QString);
+    void        setTo(const char *);
+    void        setTo(const QString);
+    void        setSubject(const char *);
+    void        setSubject(const QString);
+    void        setBody(const char *);
+    void        setBody(const QString);
+    void        addAttachment(const char *, const char *);
+    void        addAttachment(const QString, const QString);
+    void        addAttachment(const QByteArray, const QString, const QString);
+
+    int         send();
+
+protected:
+    QString                         myFrom;
+    QString                         myTo;
+    QString                         mySubject;
+    QString                         myBody;
+    QPtrList<emailAttachmentRecord>    attachmentList;
+};
+
+#endif
 
