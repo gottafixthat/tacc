3ec7b8fbbbb32435c16ceea450dc4d8dfe034e5c
marc <marc@f0b7a862-a5e2-0310-b556-ed4b80d0eeb2>
2009-03-24 23:45:16 +0000

    Updated RunSubscriptions to remove QtArch generated layout.

--- admin/RunSubscriptions.h
+++ admin/RunSubscriptions.h
@@ -1,39 +1,51 @@
-/**********************************************************************
-
-	--- Qt Architect generated file ---
-
-	File: RunSubscriptions.h
-	Last generated: Wed Aug 26 15:05:42 1998
-
- *********************************************************************/
+/**
+ * RunSubscriptions.h - Class definition for the RunSubscriptions
+ * widget.  RunSubscriptions brings up a window that gives a manager
+ * the ability to run subscriptions for all customers that have
+ * subscriptions pending.
+ *
+ * Written by R. Marc Lewis
+ *   (C)opyright 1998-2009, R. Marc Lewis and Avvatel Corporation
+ *   All Rights Reserved
+ *
+ *   Unpublished work.  No portion of this file may be reproduced in whole
+ *   or in part by any means, electronic or otherwise, without the express
+ *   written consent of Avvatel Corporation and R. Marc Lewis.
+ */
 
 #ifndef RunSubscriptions_included
 #define RunSubscriptions_included
 
-#include "RunSubscriptionsData.h"
+#include <qlabel.h>
+#include <qpushbutton.h>
+#include <qlistview.h>
+#include <TAAWidget.h>
 
-class RunSubscriptions : public RunSubscriptionsData
+class RunSubscriptions : public TAAWidget
 {
     Q_OBJECT
 
 public:
 
-    RunSubscriptions
-    (
-        QWidget* parent = NULL,
-        const char* name = NULL
-    );
-
+    RunSubscriptions(QWidget* parent = NULL, const char* name = NULL);
     virtual ~RunSubscriptions();
 
 protected slots:
     virtual void cancelAction();
     virtual void processSelections();
 
+protected:
+    QListView   *subscrList;
+    QPushButton *beginButton;
+    QPushButton *cancelButton;
+    QLabel      *totalSubscriptions;
+    QLabel      *accountsReceivable;
+    QLabel      *amountCharged;
+
 private:
     void fillList(void);
 
-    long    totSubscriptions;
+    int     totSubscriptions;
     long    totActiveLogins;
     
     int     ARAcct;
