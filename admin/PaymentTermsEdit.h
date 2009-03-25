5218fbf8df0431addcbcb9ead936f123b0b749df
marc <marc@f0b7a862-a5e2-0310-b556-ed4b80d0eeb2>
2009-03-25 20:08:28 +0000

    Removed QtArch layout from PaymentTermsEdit widget.

--- admin/PaymentTermsEdit.h
+++ admin/PaymentTermsEdit.h
@@ -1,30 +1,30 @@
-/**********************************************************************
-
-	--- Dlgedit generated file ---
-
-	File: PaymentTermsEdit.h
-	Last generated: Wed Sep 17 21:15:14 1997
-
- *********************************************************************/
+/**
+ * PaymentTermsEdit.h - Class definition to for the payment terms edit
+ * widget.
+ *
+ * Written by R. Marc Lewis
+ *   (C)opyright 1998-2009, R. Marc Lewis and Avvatel Corporation
+ *   All Rights Reserved
+ *
+ *   Unpublished work.  No portion of this file may be reproduced in whole
+ *   or in part by any means, electronic or otherwise, without the express
+ *   written consent of Avvatel Corporation and R. Marc Lewis.
+ */
 
 #ifndef PaymentTermsEdit_included
 #define PaymentTermsEdit_included
 
-#include "PaymentTermsEditData.h"
+#include <qlineedit.h>
+#include <qspinbox.h>
+#include <TAAWidget.h>
 
-class PaymentTermsEdit : public PaymentTermsEditData
+class PaymentTermsEdit : public TAAWidget
 {
     Q_OBJECT
 
 public:
 
-    PaymentTermsEdit
-    (
-        QWidget* parent = NULL,
-        const char* name = NULL,
-        int InternalID = 0
-    );
-
+    PaymentTermsEdit(QWidget* parent = NULL, const char* name = NULL, int InternalID = 0);
     virtual ~PaymentTermsEdit();
 
 public slots:
@@ -33,14 +33,21 @@ public slots:
 signals:
     void refresh(int);
     
-private slots:
+protected slots:
     virtual void savePaymentTerms();
     virtual void cancelPaymentTerms();
 
+protected:
+    QLineEdit   *desc;
+    QLineEdit   *dueDays;
+    QLineEdit   *graceDays;
+    QLineEdit   *financePct;
+    QLineEdit   *financeDays;
+    QLineEdit   *discountPct;
+    QLineEdit   *discountDays;
+
 private:
-    int *intIDIndex;      // for our combo box.
-    
-    int IntID;
+    int         myIntID;
 
 };
 #endif // PaymentTermsEdit_included
