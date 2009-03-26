11381cd7080c618e44de1d481aa7a45fd5d02f63
marc <marc@f0b7a862-a5e2-0310-b556-ed4b80d0eeb2>
2009-03-26 23:17:02 +0000

    Removed QtArch layout from OverdueAccounts widget.

--- admin/OverdueAccounts.h
+++ admin/OverdueAccounts.h
@@ -1,31 +1,33 @@
-/**********************************************************************
+/* Total Accountability Customer Care (TACC)
+ *
+ * Written by R. Marc Lewis
+ *   (C)opyright 1997-2009, R. Marc Lewis and Avvatel Corporation
+ *   All Rights Reserved
+ *
+ *   Unpublished work.  No portion of this file may be reproduced in whole
+ *   or in part by any means, electronic or otherwise, without the express
+ *   written consent of Avvatel Corporation and R. Marc Lewis.
+ */
 
-	--- Qt Architect generated file ---
 
-	File: OverdueAccounts.h
-	Last generated: Fri Aug 14 17:32:03 1998
+#ifndef OVERDUEACCOUNTS_H
+#define OVERDUEACCOUNTS_H
 
- *********************************************************************/
-
-#ifndef OverdueAccounts_included
-#define OverdueAccounts_included
-
-#include "OverdueAccountsData.h"
+#include <qlistview.h>
+#include <qdialog.h>
+#include <qlabel.h>
+#include <qradiobutton.h>
 #include <qlistview.h>
 
-class OverdueAccounts : public OverdueAccountsData
+#include <TAAWidget.h>
+
+class OverdueAccounts : public TAAWidget
 {
     Q_OBJECT
 
 public:
 
-    OverdueAccounts
-    (
-        QWidget* parent = NULL,
-        const char* name = NULL,
-        int AccountTypes = 0
-    );
-
+    OverdueAccounts(QWidget* parent = NULL, const char* name = NULL, int AccountTypes = 0);
     virtual ~OverdueAccounts();
     void    fillList(void);
 
@@ -49,5 +51,17 @@ protected slots:
     virtual void updateTotals();
     virtual void QCloseEvent(QEvent *)              { delete this; }
     
+protected:
+    QRadioButton    *noAction;
+    QRadioButton    *sendReminder;
+    QRadioButton    *suspendAccount;
+    QLabel          *accountsOverdue;
+    QLabel          *accountsSelected;
+    QLabel          *amountOverdue;
+    QLabel          *selectedOverdue;
+    QListView       *list;
 };
-#endif // OverdueAccounts_included
+
+#endif
+
+// vim: expandtab
