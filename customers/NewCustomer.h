9dfedd5fca78e2aa764d50dc4cede0cb50229551
marc <marc@f0b7a862-a5e2-0310-b556-ed4b80d0eeb2>
2009-03-30 17:51:39 +0000

    Last of the QtArch layouts removed.

--- customers/NewCustomer.h
+++ customers/NewCustomer.h
@@ -1,38 +1,62 @@
-/**********************************************************************
+/* Total Accountability Customer Care (TACC)
+ *
+ * Written by R. Marc Lewis
+ *   (C)opyright 1997-2009, R. Marc Lewis and Avvatel Corporation
+ *   All Rights Reserved
+ *
+ *   Unpublished work.  No portion of this file may be reproduced in whole
+ *   or in part by any means, electronic or otherwise, without the express
+ *   written consent of Avvatel Corporation and R. Marc Lewis.
+ */
 
-	--- Dlgedit generated file ---
+#ifndef NEWCUSTOMER_H
+#define NEWCUSTOMER_H
 
-	File: NewCustomer.h
-	Last generated: Sun Nov 2 15:45:15 1997
+#include <qlineedit.h>
+#include <qcombobox.h>
+#include <qmultilineedit.h>
 
- *********************************************************************/
+#include <TAAWidget.h>
 
-#ifndef NewCustomer_included
-#define NewCustomer_included
-
-#include "NewCustomerData.h"
-
-class NewCustomer : public NewCustomerData
+class NewCustomer : public TAAWidget
 {
     Q_OBJECT
 
 public:
 
-    NewCustomer
-    (
-        QWidget* parent = NULL,
-        const char* name = NULL
-    );
-
+    NewCustomer(QWidget* parent = NULL, const char* name = NULL);
     virtual ~NewCustomer();
 
 signals:
 	void refreshCustList();
     
-private:
+protected slots:
 	virtual void addCustomer();
-	
+
+protected:
 	int checkPhone(char * phone);
+
+    QLineEdit       *fullName;
+    QLineEdit       *contactName;
+    QLineEdit       *altContact;
+    QLineEdit       *address1;
+    QLineEdit       *address2;
+    QLineEdit       *city;
+    QLineEdit       *state;
+    QLineEdit       *zip;
+    QLineEdit       *country;
+    QLineEdit       *dayPhone;
+    QLineEdit       *evePhone;
+    QLineEdit       *faxPhone;
+    QLineEdit       *altPhone;
+    QComboBox       *ratePlan;
+    QComboBox       *statementType;
+    QComboBox       *package;
+    QComboBox       *referredBy;
+    QMultiLineEdit  *initialNotes;
 
 };
 #endif // NewCustomer_included
+
+// vim: expandtab
+
